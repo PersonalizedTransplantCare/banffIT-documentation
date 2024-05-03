19f45649bae0ead931af5bd9f7b4918e62d562b0
guifabre <guijoseph.fabre@gmail.com>
2024-05-03 14:40:49 -0400

    update documentation

---
+++ template_script.R
@@ -0,0 +1,15 @@
+#### templates ####
+
+library(banffIT)
+
+banff_dictionary <- get_banff_dictionary()
+banff_template   <- get_banff_template()
+banff_example    <- get_banff_example()
+
+path = "C:/Users/guill/OneDrive/Bureau/R/banffIT-documentation/"
+try(dir_delete(paste0(path,'docs/templates/')),silent = TRUE)
+
+write_excel_allsheets( banff_dictionary, paste0(path,"docs/templates/banff_dictionary.xlsx"))
+write_excel_allsheets( banff_template,   paste0(path,"docs/templates/banff_template.xlsx"))
+write_excel_allsheets( banff_example,    paste0(path,"docs/templates/banff_example.xlsx"))
+
